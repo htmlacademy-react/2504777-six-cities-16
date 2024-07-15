6546d84d99072712d9cbe490d0e8c2d26eca3c21
OksKuskova <oskanchik.kuskova@gmail.com>
2024-07-15 19:36:11 +0300

    Исправит ошибки линтера в index

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
 
-// const FOUND_PLACES_COUNT = 45;
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,7 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App
-      foundPlacesCount = {offers.length}
       offers={offers}
     />
   </React.StrictMode>,

a086f808046c04defafd71f226101f1c9823c1e2
OksKuskova <oskanchik.kuskova@gmail.com>
2024-08-08 14:08:41 +0300

    Расширит reducer, добавит обработку действий по загрузке списка предложений и запроса авторизационного статуса

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
-import { SixCities } from '../const';
+import { SixCities, AuthorizationStatus } from '../const';
+import { CardOffer } from '../types/offers';
 
 export const changeCity = createAction('changeCity', (value: SixCities) => ({payload: value,}));
-export const fillingOffersList = createAction('fillingOffersList');
+export const loadOffersList = createAction('loadOffersList', (value: CardOffer[]) => ({payload: value,}));
 export const changeSortingType = createAction('changeSortingType', (value: string) =>({payload: value,}));
+export const requireAuthorization = createAction('requireAuthorization', (value: AuthorizationStatus) => ({payload: value}));
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,19 +1,21 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { CardOffer } from '../types/offers';
-import { changeCity, changeSortingType } from './action';
-import { DEFAULT_CITY, SixCities, DEFAULT_SORTING_TYPE } from '../const';
-import { cardOffers } from '../mocks/offers/card-offers';
+import { changeCity, changeSortingType, loadOffersList, requireAuthorization } from './action';
+import { DEFAULT_CITY, SixCities, DEFAULT_SORTING_TYPE, AuthorizationStatus } from '../const';
+// import { cardOffers } from '../mocks/offers/card-offers';
 
 type initialStateType = {
   city: SixCities;
   offers: CardOffer[];
   sortingType: string;
+  authorizationStatus: AuthorizationStatus;
 }
 
 const initialState: initialStateType = {
   city: DEFAULT_CITY,
-  offers: cardOffers,
-  sortingType: DEFAULT_SORTING_TYPE
+  offers: [],
+  sortingType: DEFAULT_SORTING_TYPE,
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -23,5 +25,11 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(changeSortingType, (state, action) => {
       state.sortingType = action.payload;
+    })
+    .addCase(loadOffersList, (state, action) => {
+      state.offers = action.payload;
+    })
+    .addCase(requireAuthorization, (state, action) => {
+      state.authorizationStatus = action.payload;
     });
 });
